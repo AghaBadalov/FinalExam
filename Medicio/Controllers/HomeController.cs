9937688cd79aac0e47c0fbb8b1ffb77d5d143c47
AghaBadalov <6zgbdyf@code.edu.az>
2023-01-28 12:24:23 +0400

    3

--- Medicio/Controllers/HomeController.cs
+++ Medicio/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using Medicio.Models;
+﻿using Medicio.DAL;
+using Medicio.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Medicio.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _dbContext;
+
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
        
 
         public IActionResult Index()
         {
-            return View();
+            List<Doctor> doctors=_dbContext.Doctors.Include(x=>x.Profession).Where(x=>x.IsDeleted==false).ToList();
+            return View(doctors);
         }
 
         
